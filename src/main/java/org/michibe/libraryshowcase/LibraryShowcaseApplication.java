7f8975c3f095c278355bde0e6174635bcb2a498b
Michael Behr <michael.behr@novatec-gmbh.de>
2022-07-16 20:21:37 +0200

    Setup project

---
+++ src/main/java/org/michibe/libraryshowcase/LibraryShowcaseApplication.java
@@ -0,0 +1,13 @@
+package org.michibe.libraryshowcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryShowcaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryShowcaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/michibe/libraryshowcase/LibraryShowcaseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.michibe.libraryshowcase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryShowcaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
